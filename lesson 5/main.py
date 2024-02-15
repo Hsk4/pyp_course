d9a3b82dd4933a3ea1236888e2a1d941a2c17f75
Hsk4 <hsk42116@gmail.com>
2024-02-16 00:14:56 +0500

    made 18 folders  and modified lesson 5

---
+++ lesson 5/main.py
@@ -0,0 +1,63 @@
+# This fourth lesson is about Comments, Escape Sequences & Print Statement 
+
+
+
+
+                            
+
+
+# COMMENTS 
+
+# we can also use multi line comment by using ('''  hello ''' ) or ("""  hello   """)
+'''
+helllloookfjdflkdjgdjghlncghvloa  (this text will not run as it commented )
+'''
+"""
+jkkjkjkjkjkjk                      (this text will not run as it commented )
+"""
+
+
+
+# ESCAPE SEQUENCE
+
+#lets use (\n) for line break
+
+# print ("hey i am a good boy \n hello ")                
+
+
+  # NOTE: commands such as  (pressing enter to break a line ) for line break wont work so dont sweat using it ^^
+          # such as hey i am a  
+          #  good boy   
+
+# Lets use \"
+# print ("hey i am a good boy \"hello world\" \n and i and you are a good boy too ")
+
+
+
+
+#PRINT STATEMENT
+
+# a simple calculater using end and \n
+
+print ("your question"  )
+print ("5+7" ,"\n", end="answer\n")
+print ( 5+7 , ) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lesson 8/main.py
@@ -0,0 +1,7 @@
+#lets use (\n) for line break
+
+print ("hey i am a good boy \n hello ")                
+
+
+  # NOTE: commands such as hey i am a 
+#  good boy  (pressing enter to break a line ) for line break wont work so dont sweat using it ^^
