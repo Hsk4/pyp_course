0d98cc609684bc6ef6a0f10644a6bfc5573e4d51
Hsk4 <hsk42116@gmail.com>
2024-02-16 20:18:53 +0500

    renamed folder 6/7 and added content

---
+++ Variables and Data Types lesson 6/main.py
@@ -0,0 +1,46 @@
+# TOPIC OF THIS LESSON IS DATATYPES AND BUILT IN DATATYPES
+
+# INTEGER
+a = 15454545
+
+print (a)
+print ("the datatype of a is ", type (a) )
+
+
+# STRING
+b = "hammad"
+
+print (b)
+print ("the datatype of a is ", type (b) ) 
+
+
+# BOLEAN 
+c = True
+
+print (c)
+print ("the datatype of a is ", type (c) ) 
+
+
+# NULL / NONE TYPE
+d = None
+
+print (d)
+print ("the datatype of a is ", type (d ) ) 
+
+# FLOAT 
+e = 1.1
+print ("the datatype of a is ", type (e ) ) 
+
+
+
+# COMPLEX
+e = complex (8,2)
+print ("the datatype of a is ", type (e ) ) 
+print (e)
+
+
+
+
+# FLOAT 
+e = 1.1
+print ("the datatype of a is ", type (e ) ) 
--- lesson 7/main.py
+++
@@ -1,7 +0,0 @@
-#lets use (\n) for line break
-
-print ("hey i am a good boy \n hello ")                
-
-
-  # NOTE: commands such as hey i am a 
-#  good boy  (pressing enter to break a line ) for line break wont work so dont sweat using it ^^
